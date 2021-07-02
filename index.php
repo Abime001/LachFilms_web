d6ddcb80845ecd172a5940a4b853dc88e4c01282
jesusrpd <jesusrodrigo881@gmail.com>
2021-07-02 16:33:56 -0500

    Se cambio el nombre del index.js

--- index.php
+++ index.php
@@ -326,7 +326,7 @@
         </div>
 
         <!-- SCRIPTS JS DE ANIMACIONES -->
-        <script src="./index.js"></script>
+        <script src="./main.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
         <script>

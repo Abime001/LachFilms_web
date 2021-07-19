26bc22c1dcb5ffed1f2a3e73326fe1c472fb377c
jesusrpd <jesusrodrigo881@gmail.com>
2021-07-19 18:57:50 -0500

    Correccion marca de agua de los videos de la página

--- index.php
+++ index.php
@@ -273,8 +273,8 @@
                             <textarea
                                 name="message"
                                 class="question"
-                                id="msg"
-                                rows="2"
+                                id="msg" 
+                                rows="2
                                 required
                                 autocomplete="off"
                             ></textarea>

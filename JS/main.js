c0960a1e3cc0397c61750b1d70af7550742b6380
jesusrpd <jesusrodrigo881@gmail.com>
2021-07-27 13:36:18 -0500

    Se creo una página por sección del portafolio con su respectivo slide automatico y sus img

--- JS/main.js
+++ JS/main.js
@@ -1,96 +1,3 @@
-var arrow_s = document.getElementById("arrow_s");
-var arrow_p = document.getElementById("arrow_p");
-var arrow_a = document.getElementById("arrow_a");
-var arrow_t = document.getElementById("arrow_t");
-
-var social = document.getElementById("social");
-var btn_social = document.getElementById("btn-social");
-
-var producto = document.getElementById("producto");
-var btn_producto = document.getElementById("btn-producto");
-
-var aereo = document.getElementById("aereo");
-var btn_aereo = document.getElementById("btn-aereo");
-
-var turismo = document.getElementById("turismo");
-var btn_turismo = document.getElementById("btn-turismo");
-
-arrow_s.addEventListener("click", function () {
-    social.style.display = "none";
-    social.style.opacity = 0;
-});
-
-arrow_p.addEventListener("click", function () {
-    producto.style.display = "none";
-    producto.style.opacity = 0;
-});
-
-arrow_a.addEventListener("click", function () {
-    aereo.style.display = "none";
-    aereo.style.opacity = 0;
-});
-
-arrow_t.addEventListener("click", function () {
-    turismo.style.display = "none";
-    turismo.style.opacity = 0;
-});
-
-btn_social.addEventListener("click", function () {
-    social.style.display = "block";
-    social.style.opacity = 1;
-
-    turismo.style.display = "none";
-    turismo.style.opacity = 0;
-
-    aereo.style.display = "none";
-    aereo.style.opacity = 0;
-
-    producto.style.display = "none";
-    producto.style.opacity = 0;
-});
-
-btn_producto.addEventListener("click", function () {
-    social.style.display = "none";
-    social.style.opacity = 0;
-
-    turismo.style.display = "none";
-    turismo.style.opacity = 0;
-
-    aereo.style.display = "none";
-    aereo.style.opacity = 0;
-
-    producto.style.display = "block";
-    producto.style.opacity = 1;
-});
-
-btn_aereo.addEventListener("click", function () {
-    aereo.style.display = "block";
-    aereo.style.opacity = 1;
-
-    social.style.display = "none";
-    social.style.opacity = 0;
-
-    turismo.style.display = "none";
-    turismo.style.opacity = 0;
-
-    producto.style.display = "none";
-    producto.style.opacity = 0;
-});
-
-btn_turismo.addEventListener("click", function () {
-    aereo.style.display = "none";
-    aereo.style.opacity = 0;
-
-    social.style.display = "none";
-    social.style.opacity = 0;
-
-    turismo.style.display = "block";
-    turismo.style.opacity = 1;
-
-    producto.style.display = "none";
-    producto.style.opacity = 0;
-});
-
 btn__menu_nav.addEventListener( "click", function() {
     $(".toggle").on("click", function() {
         if ($(".item").hasClass("active")) {
@@ -121,4 +28,46 @@ x.onclick = function () {
     window.scrollTo(0, 0);
 };
 
+//ANIMACIÓN ENTRE PÁGINAS
+barba.init({
+    transitions: [
+        {
+            name: "page",
+            leave: function (data) {
+                var done = this.async();
+                document.body.classList.add("loading");
+                setTimeout(function () {
+                    done();
+                }, 900);
+            },
+            enter: function (data) {
+                var done = this.async();
+                document.body.classList.add("loading");
+                setTimeout(function () {
+                    done();
+                }, 900);
+            },
+        },
+    ],
+});
 
+
+//SLIDER
+const swiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 30,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
